0bd61bf3ca448fa927c07eb50b10d50b89faaf62
Lee, yeeun <ye7426@naver.com>
2024-09-25 09:47:11 +0900

    fourth

--- e202214046/Main.java
+++ e202214046/Main.java
@@ -1,13 +1,10 @@
 public class Main {
 
-    public static void main(String[] args) {
-<<<<<<< HEAD
-      System.out.println("이예은");
-=======
-      System.out.println("홍길동");
->>>>>>> f54492505d883f9c762eb9a0d1bcff47ae60f1f2
-      World.run();
+      public static void main(String[] args) {
+        System.out.println("이예은");
+        World.run();
+        Hello.run();
+      }
+    
     }
-  
-  }
-  
+    
